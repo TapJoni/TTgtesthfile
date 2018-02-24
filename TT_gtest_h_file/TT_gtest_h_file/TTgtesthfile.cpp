6c4697baf7205020f09ca1468a115b489cd49a1b
Tapio Mattila <tapio.mattila@edu.turkuamk.fi>
2018-02-24 14:52:34 +0200

    Added test VS project that includes gtest files

---
+++ TT_gtest_h_file/TT_gtest_h_file/TTgtesthfile.cpp
@@ -0,0 +1,13 @@
+// TTgtesthfile.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+// SimpleMath.cpp : Defines the entry point for the console application.
+#include "simplemath.h"
+
+int main()
+{
+	cubic(10);
+	return 0;
+}
+
